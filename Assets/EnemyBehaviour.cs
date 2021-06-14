b5352f34b8278564cb6fa8ce17a28e340ffd5c09
Justin-Ong <justinong32@gmail.com>
2021-06-14 14:43:31 +0800

    Fix bug where enemies would get stuck on dead enemies instead of continuing to chase the player.

--- Assets/EnemyBehaviour.cs
+++ Assets/EnemyBehaviour.cs
@@ -28,10 +28,9 @@ public class EnemyBehaviour : MonoBehaviour
         if (!isDead)
         {
             Vector3 vectorToPlayer = References.player.transform.position - transform.position;
-            Vector3 distance = vectorToPlayer.normalized * speed * Time.deltaTime;
-            Vector3 newPos = transform.position + distance;
+            Vector3 distance = vectorToPlayer.normalized * speed;
             if (Physics.RaycastNonAlloc(transform.position, vectorToPlayer, collisionDetectionArray, distance.magnitude, enemyLayer) == 0) {
-                ourBody.MovePosition(newPos);
+                ourBody.velocity = distance;
             }
         }
     }
@@ -66,19 +65,11 @@ public class EnemyBehaviour : MonoBehaviour
     public void Die(bool countScore)
     {
         GameController.controller.SpawnEnemies();
-        /*
-        int numSpawns = Random.Range(1, 3);
-        for (int i = 0; i < numSpawns; i++) {
-            GameObject temp = Instantiate(gameObject, new Vector3(Random.Range(-24, 24), 1, Random.Range(-24, 24)), transform.rotation);
-            temp.GetComponent<EnemyBehaviour>().enemyPrefab = enemyPrefab;
-            temp.GetComponent<EnemyBehaviour>().currHealth = maxHealth;
-            temp.GetComponent<EnemyBehaviour>().speed = References.maxSpeed;
-        }
-        */
         if (countScore)
         {
             References.score += 1;
         }
         isDead = true;
+        gameObject.layer = LayerMask.NameToLayer("Dead");
     }
 }
